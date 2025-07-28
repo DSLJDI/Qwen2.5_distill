3f5b409f7072ca7356d4e35d914fd2afa2beac09
DSLJDI <54802522+DSLJDI@users.noreply.github.com>
2025-07-28 19:11:34 +0800

    Update run_train.sh

--- run_train.sh
+++ run_train.sh
@@ -10,7 +10,7 @@ PROJECT_DIR=$(dirname "$(realpath "$0")")
 TRAIN_DATA="$PROJECT_DIR/data/train_data.json"
 VAL_DATA="$PROJECT_DIR/data/test.data.json"
 OUTPUT_DIR="$PROJECT_DIR/cot_distill_results_7B_model"
-ORIGINAL_MODEL="Qwen/Qwen2.5-0.5B-Instruct"
+ORIGINAL_MODEL="Qwen/Qwen2.5-7B-Instruct"
 
 # --- 创建目录 ---
 mkdir -p "$OUTPUT_DIR/"
@@ -25,18 +25,18 @@ torchrun --nproc_per_node=1 --master_port 29512 \
     --output_dir "$OUTPUT_DIR" \
     --logging_dir "$OUTPUT_DIR/logs" \
     --deepspeed "$PROJECT_DIR/configs/deepspeed_config.json" \
-    --num_train_epochs 3 \
+    --num_train_epochs 2 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 2 \
     --gradient_accumulation_steps 8 \
-    --learning_rate 2e-6 \
+    --learning_rate 5e-5 \
     --weight_decay 0.01 \
     --warmup_ratio 0.05 \
     --lr_scheduler_type "cosine" \
     --logging_steps 10 \
     --save_strategy "epoch" \
     --eval_strategy "epoch" \
-    --save_total_limit 3 \
+    --save_total_limit 2 \
     --report_to "tensorboard" \
     --train_max_length 4300 \
     --fp16 \
